4af89d0e248a9b71324b76089ad62b38f1948788
Diego Cid Merino <diegocidm4@hotmail.com>
2024-05-17 22:34:51 +0200

    Versión inicial

---
+++ Sources/eIDReader.xcframework/ios-arm64_x86_64-simulator/eIDReader.framework/Headers/eIDReader.h
@@ -0,0 +1,18 @@
+//
+//  eIDReader.h
+//  eIDReader
+//
+//  Created by Diego Cid Merino on 14/4/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for eIDReader.
+FOUNDATION_EXPORT double eIDReaderVersionNumber;
+
+//! Project version string for eIDReader.
+FOUNDATION_EXPORT const unsigned char eIDReaderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <eIDReader/PublicHeader.h>
+
+
